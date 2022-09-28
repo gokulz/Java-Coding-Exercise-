823713a07f14860a28fd45aba13b3e398d3fefa6
Gokul MS <75795509+gokulz@users.noreply.github.com>
2022-09-28 10:01:41 +0530

    Update DecimalComparator.java

--- DecimalComparator.java
+++ DecimalComparator.java
@@ -1,10 +1,31 @@
+/*
+Write a method areEqualByThreeDecimalPlaces with two parameters of type double.
+
+The method should return boolean and it needs to return true if two double numbers are the same up to three decimal places. Otherwise, return false.
+
+EXAMPLES OF INPUT/OUTPUT:
+
+areEqualByThreeDecimalPlaces(-3.1756, -3.175); → should return true since numbers are equal up to 3 decimal places.
+
+areEqualByThreeDecimalPlaces(3.175, 3.176); → should return false since numbers are not equal up to 3 decimal places
+
+areEqualByThreeDecimalPlaces(3.0, 3.0); → should return true since numbers are equal up to 3 decimal places.
+
+areEqualByThreeDecimalPlaces(-3.123, 3.123); → should return false since numbers are not equal up to 3 decimal places.
+*/
+//Tip: use casting
+
+
 public class DecimalComparator {
     public static void main(String[] args) {
         System.out.println(areEqualByThreeDecimalPlaces(-3.1756,-3.175));
 
     }
     public static boolean areEqualByThreeDecimalPlaces(double num1, double num2){
-        int n1 = (int) (num1 *1000);
+        
+        //casting in java helps to change one data types into another datatypes
+        
+        int n1 = (int) (num1 *1000); //casting boolean value to int value 
         int n2 = (int) (num2 *1000);
         if(n1 == n2){
             return true;
