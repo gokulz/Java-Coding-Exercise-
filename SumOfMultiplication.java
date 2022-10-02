09e93f92ff596654800ac20c60a442e2e3677245
Gokul MS <75795509+gokulz@users.noreply.github.com>
2022-10-02 22:05:30 +0530

    Add files via upload

---
+++ SumOfMultiplication.java
@@ -0,0 +1,20 @@
+public class SumOfMultiplication {
+    public static void main(String[] args) {
+        int sum=0;
+        int count=0;
+        //using range from 1-1000
+        for(int i=1; i<=1000; i++){
+            //Numbers that can divided by both 3 and 5
+            if(i%3==0 && i%5==0){
+                count++;
+                sum=sum+i;
+               System.out.println(i);
+               //using only five numbers for above condition
+                if(count==5){
+                    break;
+                  }
+            }
+        }
+        System.out.println(sum);
+    }
+}
